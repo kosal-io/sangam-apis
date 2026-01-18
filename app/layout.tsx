b5a148caf2dc1348a0edac7f2882aa4eaebb00e4
Manoj Prabhakar <134469946+buddhathemanoj@users.noreply.github.com>
2026-01-18 23:10:20 +0530

    chore: add layout.tsx (TypeScript)

---
+++ app/layout.tsx
@@ -0,0 +1,2 @@
+export const metadata = { title: 'Sangam APIs' };
+export default function RootLayout({ children }: { children: React.ReactNode }) { return (<html lang="en"><body>{children}</body></html>)}
